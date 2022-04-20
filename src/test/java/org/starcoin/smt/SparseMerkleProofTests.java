a4e635b386c8b755754e1905838609414a237cdf
yangjiefeng <yangjiefeng@gmail.com>
2022-04-20 15:58:48 +0800

    add test code

--- src/test/java/org/starcoin/smt/SparseMerkleProofTests.java
+++ src/test/java/org/starcoin/smt/SparseMerkleProofTests.java
@@ -76,16 +76,33 @@ public class SparseMerkleProofTests {
         proof = smt.prove("testKey5".getBytes(StandardCharsets.UTF_8));
         Assertions.assertTrue(SparseMerkleProof.verifyProof(proof, newRoot, "testKey5".getBytes(StandardCharsets.UTF_8), polyTxExistsValue, smt.getTreeHasher().getHasher()));
 
+        //        try {
+        //            smt.proveNonMembershipForRoot(new Bytes("testKey5".getBytes(StandardCharsets.UTF_8)), smt.getRoot());
+        //            smt.proveMembershipForRoot(new Bytes("bar".getBytes(StandardCharsets.UTF_8)), smt.getRoot());
+        //        } catch (SparseMerkleTree.MembershipProofException e) {
+        //            e.printStackTrace();
+        //        }
+
         // test prove non-membership and membership
         Assertions.assertThrows(SparseMerkleTree.NonMembershipProofException.class,
                 () -> smt.proveNonMembershipForRoot(new Bytes("testKey5".getBytes(StandardCharsets.UTF_8)), smt.getRoot()));
         Assertions.assertThrows(SparseMerkleTree.MembershipProofException.class,
                 () -> smt.proveMembershipForRoot(new Bytes("bar".getBytes(StandardCharsets.UTF_8)), smt.getRoot()));
-//        try {
-//            smt.proveNonMembershipForRoot(new Bytes("testKey5".getBytes(StandardCharsets.UTF_8)), smt.getRoot());
-//            smt.proveMembershipForRoot(new Bytes("bar".getBytes(StandardCharsets.UTF_8)), smt.getRoot());
-//        } catch (SparseMerkleTree.MembershipProofException e) {
-//            e.printStackTrace();
-//        }
+
     }
+
+    @Test
+    public void testUpdateRoot_1() {
+        byte[] path = HexUtils.hexToByteArray("8b4a296734b97f3c2028326c695f076e35de3183ada9d07cb7b9a32f1451d71f");
+        byte[] value = polyTxExistsValue;
+        Bytes[] sideNodes = new Bytes[]{
+                new Bytes(HexUtils.hexToByteArray("6f9bb267d56d0feecdd121f682df52b22d366fa7652975bec3ddabe457207eab"))
+        };
+        byte[] oldLeafData = HexUtils.hexToByteArray("0080be6638e99f15d7942bd0130b9118125010293dcc2054fdbf26bf997d0173f42767f15c8af2f2c7225d5273fdd683edc714110a987d1054697c348aed4e6cc7");
+        Bytes r = SparseMerkleProof.updateRootByPath(new TreeHasher(new Sha3Digest256Hasher()), new Bytes(path), new Bytes(value),
+                sideNodes, new Bytes(oldLeafData));
+        System.out.println(HexUtils.byteArrayToHex(r.getValue()));
+        Assertions.assertEquals(new Bytes(HexUtils.hexToByteArray("755e48a4526b0c5b3f7e26d00da398ffec97dc784777e16132681aa208b16be3")), r);
+    }
+
 }
--- src/test/java/org/starcoin/smt/SparseMerkleTreeTests.java
+++ src/test/java/org/starcoin/smt/SparseMerkleTreeTests.java
@@ -32,20 +32,4 @@ public class SparseMerkleTreeTests {
         System.out.println(HexUtils.byteArrayToHex(smt.getRoot().getValue()));
     }
 
-//    @Test
-//    public void testUpdateRoot_1() {
-//        byte[] path = HexUtils.hexToByteArray("8b4a296734b97f3c2028326c695f076e35de3183ada9d07cb7b9a32f1451d71f");
-//        byte[] value = polyTxExistsValue;
-//        Bytes[] sideNodes = new Bytes[]{
-//                new Bytes(HexUtils.hexToByteArray("6f9bb267d56d0feecdd121f682df52b22d366fa7652975bec3ddabe457207eab"))
-//        };
-////
-////        byte[] oldLeafData = HexUtils.hexToByteArray("0080be6638e99f15d7942bd0130b9118125010293dcc2054fdbf26bf997d0173f42767f15c8af2f2c7225d5273fdd683edc714110a987d1054697c348aed4e6cc7");
-////        SparseMerkleTree smt = new SparseMerkleTree(nodes, values);
-////        r, err :=smt.updateForRoot(path, value, sideNodes, oldLeafData)
-////        if err != nil {
-////            t.FailNow()
-////        }
-////        fmt.Println(hex.EncodeToString(r)) //755e48a4526b0c5b3f7e26d00da398ffec97dc784777e16132681aa208b16be3
-//    }
 }
